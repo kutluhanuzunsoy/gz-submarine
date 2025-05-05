6f34ff4bdd3fe63308e7e937261e0342c4957391
kutluhanuzunsoy <kutluhanuzunsoy@hotmail.com>
2025-05-05 17:17:09 +0300

    Initial folder structure and package setup

---
+++ ros2_ws/src/submarine_bringup/launch/bringup.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ]),
+        launch_arguments={'gz_args': '-r empty.sdf'}.items()
+    )
+
+    bridges = [
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='cmd_vel_bridge',
+            arguments=['/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'],
+            output='screen'
+        ),
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='camera_bridge',
+            arguments=['/camera/image_raw@sensor_msgs/msg/Image@gz.msgs.Image'],
+            output='screen'
+        )
+    ]
+
+    return LaunchDescription([gazebo_launch] + bridges)
