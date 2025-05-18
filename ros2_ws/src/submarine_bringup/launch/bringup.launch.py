4b495ff4b27440cbba5810ac42976235e4f39064
kutluhanuzunsoy <kutluhanuzunsoy@hotmail.com>
2025-05-18 18:51:43 +0300

    Add world, model, and teleop controls

--- ros2_ws/src/submarine_bringup/launch/bringup.launch.py
+++ ros2_ws/src/submarine_bringup/launch/bringup.launch.py
@@ -1,33 +1,64 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    gazebo_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
-        ]),
-        launch_arguments={'gz_args': '-r empty.sdf'}.items()
+    pkg_submarine_gazebo_share = get_package_share_directory('submarine_gazebo')
+    world_path = os.path.join(pkg_submarine_gazebo_share, 'worlds', 'submarine_world.sdf')
+
+    submarine_models_path = os.path.join(pkg_submarine_gazebo_share, 'models')
+    current_gz_sim_resource_path = os.environ.get('GZ_SIM_RESOURCE_PATH', '')
+
+    if current_gz_sim_resource_path:
+        new_gz_sim_resource_path = submarine_models_path + os.pathsep + current_gz_sim_resource_path
+    else:
+        new_gz_sim_resource_path = submarine_models_path
+    
+    set_gazebo_resource_path_action = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=new_gz_sim_resource_path
     )
 
-    bridges = [
-        Node(
-            package='ros_gz_bridge',
-            executable='parameter_bridge',
-            name='cmd_vel_bridge',
-            arguments=['/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'],
-            output='screen'
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
         ),
-        Node(
-            package='ros_gz_bridge',
-            executable='parameter_bridge',
-            name='camera_bridge',
-            arguments=['/camera/image_raw@sensor_msgs/msg/Image@gz.msgs.Image'],
-            output='screen'
-        )
-    ]
+        launch_arguments={'gz_args': f'-r {world_path}'}.items()
+    )
 
-    return LaunchDescription([gazebo_launch] + bridges)
+    cmd_vel_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='cmd_vel_bridge',
+        arguments=['/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'],
+        output='screen'
+    )
+
+    camera_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='camera_bridge',
+        arguments=['/camera/image_raw@sensor_msgs/msg/Image@gz.msgs.Image'],
+        output='screen'
+    )
+    
+    teleop_node = Node(
+        package='teleop_twist_keyboard',
+        executable='teleop_twist_keyboard',
+        name='teleop_twist_keyboard',
+        prefix='xterm -e',
+        output='screen',
+        remappings=[('/cmd_vel', '/cmd_vel')]
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(set_gazebo_resource_path_action)
+    ld.add_action(gazebo_launch)
+    ld.add_action(cmd_vel_bridge)
+    ld.add_action(camera_bridge)
+    ld.add_action(teleop_node)
+
+    return ld
--- ros2_ws/src/submarine_control/setup.py
+++ ros2_ws/src/submarine_control/setup.py
@@ -17,7 +17,6 @@ setup(
     maintainer_email='k@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
         ],
